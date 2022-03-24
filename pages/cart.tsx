8e16ebe7c7f4361d17afb0c558b837e5243d0db0
Adam <rumpelteam@gmail.com>
2022-03-24 18:18:29 +0100

    Add cart

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -11,27 +11,29 @@ const CartContent = () => {
             <span>
               {item.count} x {item.title}
             </span>
-            <span>{item.price}</span>
-            <button
-              onClick={() => cartState.removeItemFromCart(item.id)}
-              className="ml-4 text-red-500 flex"
-              aria-label="Usuń element"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
+            <div className="flex">
+              <span className="ml-2">{item.price}</span>
+              <button
+                onClick={() => cartState.removeItemFromCart(item.id)}
+                className="ml-4 text-red-500 flex"
+                aria-label="Usuń element"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            </div>
           </li>
         ))}
       </ul>
